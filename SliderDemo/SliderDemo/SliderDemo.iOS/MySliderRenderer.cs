8eaa55f52165b845059e5cd657e1e3d8af3c7d98
Cole Xia <cole_xia@hotmail.com>
2017-08-23 10:50:19 +0800

    11

---
+++ SliderDemo/SliderDemo/SliderDemo.iOS/MySliderRenderer.cs
@@ -0,0 +1,20 @@
+﻿using SliderDemo;
+using SliderDemo.iOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MySlider), typeof(MySliderRenderer))]
+namespace SliderDemo.iOS
+{
+    class MySliderRenderer : SliderRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Slider> e)
+        {
+            SetNativeControl(new MySlideriOS());
+            base.OnElementChanged(e);
+        }
+    }
+}
---
+++ SliderDemo/SliderDemo/SliderDemo.iOS/MySlideriOS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using CoreGraphics;
+
+namespace SliderDemo.iOS
+{
+    public class MySlideriOS : UISlider
+    {
+        public MySlideriOS()
+        {
+            this.MinimumTrackTintColor = UIColor.FromRGB(0xE6, 0x00, 0x06);
+            this.ThumbTintColor = UIColor.Red;
+            this.MinimumTrackTintColor = UIColor.Orange;
+            this.MaximumTrackTintColor = UIColor.Yellow;
+        }
+
+        public override CGRect TrackRectForBounds(CGRect forBounds)
+        {
+            CGRect rect = base.TrackRectForBounds(forBounds);
+            return new CGRect(rect.X, rect.Y, rect.Width, 20);
+        }
+    }
+}
